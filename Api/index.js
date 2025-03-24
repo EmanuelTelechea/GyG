56036c948eabdfcd66121ee7f7c848d665e359f1
Emanuel Telechea <emanueltelechea7@gmail.com>
2025-03-24 19:08:58 -0300

    Actualizar configuración de conexión a la base de datos en .env y index.js

--- Api/index.js
+++ Api/index.js
@@ -15,10 +15,10 @@ const PORT = 3000;
 
 // Configurar conexión a MySQL con pool
 const pool = mysql.createPool({
-    host: `localhost`, 
-    user: `root`,
-    password: `root`,
-    database: `GGart_db`
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 // Middleware
