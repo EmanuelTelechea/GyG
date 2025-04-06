4c96c17afe1db2e00d91f0f6731988f6c68e5803
Emanuel Telechea <emanueltelechea7@gmail.com>
2025-04-06 15:15:27 -0300

    Agregar prefijo 'http://' a las URLs de la API en los archivos de contacto, productos y detalle del producto

--- Web/Dominio/contacto.js
+++ Web/Dominio/contacto.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', function() {
         const phone = formData.get('phone');
         const message = formData.get('message');
 
-        fetch('gyg-production.up.railway.app/api/contacto', {
+        fetch('http://gyg-production.up.railway.app/api/contacto', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
